e921267102cf48b1a8286c4d23123bf93bb45031
Redoxahmii <fullmetal2334@gmail.com>
2023-08-02 02:13:49 +0500

    better

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,12 +16,14 @@ const Home = () => {
 
   return (
     <>
-      <div className="w-screen mt-56 flex items-center justify-center">
+      <div className="w-screen mt-40 flex items-center justify-center">
         <div className=" flex flex-col items-center justify-center gap-4">
-          <h1 className="text-7xl tracking-tighter ">
-            Welcome to Movie Database
-          </h1>
-          <div className=" w-full   max-w-3xl text-center">
+          <div className="bg-gradient-to-r from-blue-300 to-purple-500 bg-clip-text">
+            <h1 className="text-7xl tracking-tighter text-transparent">
+              Welcome to Movie Database
+            </h1>
+          </div>
+          <div className=" w-full max-w-3xl text-center pt-4 mb-2">
             <p className=" text-2xl tracking-tight font-serif">
               A free Frontend Client for watching Movies. Search for your
               favourite movies and watch them for free.
@@ -29,12 +31,13 @@ const Home = () => {
           </div>
           <input
             type="text"
-            className="input input-primary"
+            placeholder="Search for movies"
+            className="input input-bordered w-96 border-purple-400"
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
       </div>
-      <div className="flex flex-wrap w-screen pl-10  gap-5 mt-20">
+      <div className="flex flex-wrap w-screen pl-10 gap-5 mt-20">
         {Response.map((movie, index) => (
           <div key={index}>
             <Card movie={movie}></Card>
