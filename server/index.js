3f8dc61940a19b6ffb2533a78f0706c74774f5f5
Redoxahmii <fullmetal2334@gmail.com>
2023-09-25 22:42:57 +0500

    cjs

--- server/index.js
+++ server/index.js
@@ -6,59 +6,44 @@ import MovieRoutes from "./routes/Movie.js";
 import TvShowRoutes from "./routes/Shows.js";
 import UserRoutes from "./routes/User.js";
 import cookieParser from "cookie-parser";
+import helmet from "helmet/index.cjs";
 
-let helmet;
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-if (process.env.HELMET === "true") {
-  import("helmet").then((module) => {
-    helmet = module.default;
-    initializeServer();
-  });
-} else {
-  import("helmet/index.cjs").then((module) => {
-    helmet = module.default;
-    initializeServer();
-  });
-}
+// Specify the IP address (127.0.0.1) and port (e.g., 5000)
+const serverAddress = process.env.ORIGIN;
+const serverPort = process.env.PORT || 5000;
+app.use(
+  cors({
+    // Use the serverAddress variable for the origin
+    origin: `${serverAddress}`,
+    credentials: true,
+  })
+);
 
-function initializeServer() {
-  dotenv.config();
-  const app = express();
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-  const serverAddress = process.env.ORIGIN;
-  const serverPort = process.env.PORT || 5000;
-  app.use(
-    cors({
-      origin: `${serverAddress}`,
-      credentials: true,
-    })
-  );
+// Routes
 
-  app.use(cookieParser());
+app.use("/api/movie", MovieRoutes);
+app.use("/api/tvshow", TvShowRoutes);
+app.use("/api/user", UserRoutes);
 
-  // Ensure that helmet is a function before using it
-  if (typeof helmet === "function") {
-    app.use(helmet());
-    app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-  }
-
-  app.use("/api/movie", MovieRoutes);
-  app.use("/api/tvshow", TvShowRoutes);
-  app.use("/api/user", UserRoutes);
-
-  mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      app.listen(serverPort, () => {
-        console.log(`Server is running on ${serverPort}`);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(serverPort, () => {
+      console.log(`Server is running on ${serverPort}`);
     });
-}
+  })
+  .catch((err) => {
+    console.log(err);
+  });
